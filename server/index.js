05c672084cb0f7fadef719bef0d0c668a4a74653
sharleenawinja <awinjasharleen@gmail.com>
2024-11-05 11:23:07 +0300

    create visit endpoint

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { patientRouter } from "./routes/patient.js";
+import { visitRouter } from "./routes/visit.js";
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", patientRouter);
+app.use("/api", visitRouter);
 
 app.listen(PORT, () => {
   console.log("server is running");
---
+++ server/routes/visit.js
@@ -0,0 +1,56 @@
+import express from "express";
+import con from "../utils/db.js";
+
+const router = express.Router();
+
+router.post("/visit", (req, res) => {
+  const {
+    patientId,
+    date,
+    height,
+    weight,
+    bmi,
+    generalHealth,
+    dietedBefore,
+    takingDrugs,
+    comments,
+  } = req.body;
+
+  const sql =
+    "INSERT INTO Visits (patient_id, entry_date, height, weight, bmi, general_health, currently_taking_any_drugs, ever_been_on_weight_loss_diet, comments) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+  con.query(
+    sql,
+    [
+      patientId,
+      date,
+      height,
+      weight,
+      bmi,
+      generalHealth || null,
+      takingDrugs || null,
+      dietedBefore || null,
+      comments || null,
+    ],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          success: false,
+          message: "Internal server error",
+          error: err,
+        });
+      }
+
+      const { insertId } = result;
+
+      return res.status(201).json({
+        success: true,
+        message: "Visit successfully recorded.",
+        patientId: insertId,
+      });
+    }
+  );
+});
+
+export { router as visitRouter };
